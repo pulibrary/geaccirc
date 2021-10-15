c61f857d6aaef5bf79a61bde1a4b3c0123c4d0f2
Carolyn Cole <cac9@princeton.edu>
2021-10-15 11:34:40 -0400

    Generated rails application and Record scaffold rails generate scaffold Record barcode:string author:string title:string call_no:string sub_loc:string location:string copy:string status:string

---
+++ app/views/records/_record.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! record, :id, :barcode, :author, :title, :call_no, :sub_loc, :location, :copy, :status, :created_at, :updated_at
+json.url record_url(record, format: :json)
---
+++ db/migrate/20211015153403_create_records.rb
@@ -0,0 +1,16 @@
+class CreateRecords < ActiveRecord::Migration[5.2]
+  def change
+    create_table :records do |t|
+      t.string :barcode
+      t.string :author
+      t.string :title
+      t.string :call_no
+      t.string :sub_loc
+      t.string :location
+      t.string :copy
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end

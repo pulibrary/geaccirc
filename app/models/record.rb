a22d53d98a1939457198a3ed50b138fa2e5164cb
Jane Sandberg <js7389@princeton.edu>
2023-04-25 09:16:57 -0700

    Remove codeql sql injection concern

--- app/models/record.rb
+++ app/models/record.rb
@@ -6,6 +6,6 @@ class Record < ApplicationRecord
   def self.search(term:, field: 'title')
     return Record.all if term.blank?
 
-    Record.where("#{field} ilike ?", "%#{term}%")
+    Record.where(Record.arel_table[field.to_sym].matches("%#{term}%"))
   end
 end
